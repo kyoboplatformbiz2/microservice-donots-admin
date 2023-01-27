899df46cd74420f705e60db9eaaf6c0ecb2b8726
이로얌 <wlfekymk@gmail.com>
2023-01-27 18:49:05 +0900

    [common] faq 버그 픽스

--- src/main/java/com/kyobo/platform/donots/model/entity/FaqPost.java
+++ src/main/java/com/kyobo/platform/donots/model/entity/FaqPost.java
@@ -7,32 +7,24 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Builder
-@Data
 @Table(name = "FAQ_POST")
 public class FaqPost {
 
     @Id
+    @GeneratedValue
     @Column(name = "faq_post_key")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long key;
 
-    @Column(name = "FAQ_CATEGORY", nullable = false)
     private FaqCategory faqCategory;
-
-    @Column(name = "QUESTION", nullable = false)
     private String question;
-    @Column(name = "ANSWER", nullable = false)
     private String answer;
-    @Column(name = "REPRESENTATIVE_IMG_URL", nullable = false)
     private String representativeImgUrl;
-
-    @Column(name = "CREATED_DATETIME", nullable = false)
     private LocalDateTime createdDatetime;
-    @Column(name = "LAST_MODIFIED_DATETIME", nullable = false)
     private LocalDateTime lastModifiedDatetime;
     public void updateFaqPost(FaqPostRequest faqPostRequest) {
         LocalDateTime now = LocalDateTime.now();
@@ -43,4 +35,3 @@ public class FaqPost {
         this.lastModifiedDatetime = now;
     }
 }
-
