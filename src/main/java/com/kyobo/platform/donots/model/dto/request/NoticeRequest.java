74d5be6d8fe743a9117b6578023094f76f7e5f2f
juny <junhee.dev@daum.net>
2023-02-01 21:50:20 +0900

    [어드민 공지사항, FAQ 시간 형식 요청] 어드민에서 공지사항, FAQ 메뉴에서
    공지사항 등록 API
    공지사항 수정 API
    
    FAQ 등록 API
    
    상기 3개 API에서 시간 방식 변경을 요청 드립니다.
    
    현재방식
    2023-01-19T00:00:00
    
    변경요청방식
    2023-01-19 00:00:00

--- src/main/java/com/kyobo/platform/donots/model/dto/request/NoticeRequest.java
+++ src/main/java/com/kyobo/platform/donots/model/dto/request/NoticeRequest.java
@@ -1,8 +1,10 @@
 package com.kyobo.platform.donots.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -14,22 +16,21 @@ public class NoticeRequest {
     }
 
     @Schema(description = "제목")
-    @NotNull
+    @NotBlank
     private String title;
     
     @Schema(description = "본문")
-    @NotNull
+    @NotBlank
     private String body;
     
     @Schema(description = "이미지 주소")
-    @NotNull
     private String imgUrl;
 
     @Schema(description = "게시 시작")
-    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime boardStartDate;
 
     @Schema(description = "게시 종료")
-    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime boardEndDate;
 }
