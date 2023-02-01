9098c67982b7046c3a6d3ae5fa1954873c9c834c
juny <junhee.dev@daum.net>
2023-02-01 11:25:49 +0900

    FAQ 유형에 기획문서 반영

--- src/main/java/com/kyobo/platform/donots/model/entity/FaqCategory.java
+++ src/main/java/com/kyobo/platform/donots/model/entity/FaqCategory.java
@@ -1,12 +1,14 @@
 package com.kyobo.platform.donots.model.entity;
 
 public enum FaqCategory {
-    NEW_FEATURE("신규기능"),
-    SIGN_IN("로그인"),
+    GENERAL("일반"),
+    SIGN_UP_AND_SIGN_IN("가입/로그인"),
     RECIPE_POST("레시피"),
+    BABY_PROFILE("아이 프로필"),
+    MEMBER_RANKING_BOARD("랭킹"),
     MISCELLANY("기타");
 
-    public String name;
+    public final String name;
 
     FaqCategory(String name) {
         this.name = name;
