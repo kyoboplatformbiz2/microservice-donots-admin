240fcecc1e7265ec58475eeabfad800e42de5352
juny <junhee.dev@daum.net>
2023-02-22 16:13:04 +0900

    [bodyHtmlFileUrl 컬럼 추가 대응]

--- src/main/java/com/kyobo/platform/donots/model/repository/TermsOfServiceRepository.java
+++ src/main/java/com/kyobo/platform/donots/model/repository/TermsOfServiceRepository.java
@@ -13,7 +13,7 @@ public interface TermsOfServiceRepository extends JpaRepository<TermsOfService, 
 
     @Query(value = "SELECT * " +
             "FROM ( " +
-            "   SELECT terms_of_service_key, admin_id, body, created_datetime, last_modified_datetime, posting_end_datetime, posting_start_datetime, title, version, " +
+            "   SELECT terms_of_service_key, admin_id, body, body_html_file_url, created_datetime, last_modified_datetime, posting_end_datetime, posting_start_datetime, title, version, " +
             "   RANK() OVER(PARTITION BY title ORDER BY created_datetime DESC) FROM terms_of_service " +
             ") a " +
             "WHERE rank = 1",
